71260c6ee0c1661392779a906fc48de602beed6c
Tara1903 <tarasinghsikh1903@gmail.com>
2026-08-16 21:47:33 +0530

    fix: SQL column name for product images

--- supabase/migrations/00016_demo_images.sql
+++ supabase/migrations/00016_demo_images.sql
@@ -2,7 +2,4 @@
 -- Sets the diamond trial pack image as a temporary demo placeholder for all products
 
 UPDATE product_images 
-SET image_url = '/images/products/diamond-trial.jpg';
-
-UPDATE product_variants 
-SET image_url = '/images/products/diamond-trial.jpg';
+SET url = '/images/products/diamond-trial.jpg';
